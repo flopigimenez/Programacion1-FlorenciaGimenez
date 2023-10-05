ec68d8ce07b5682cd11ac3ddabed292c711837a2
flopigimenez <flopigmzm@gmail.com>
2023-10-05 11:34:26 -0300

    Agregar archivos

---
+++ Ejercicio Complementario1.py
@@ -0,0 +1,73 @@
+#Ejercicio 1
+numero1 = 8 
+print("Numero 1: ", numero1)
+
+#Ejercicio 2       
+numero2 = 5
+print("Numero 2: ", numero2)
+
+#Ejercicio 3
+suma = numero1 + numero2
+print("Suma: ", suma)
+
+#Ejercicio 4
+resta = numero1 - numero2
+mult = numero1 * numero2
+division = numero1 / numero2
+print("Resta: ", resta)
+print("Multiplicacion: ", mult)
+print("diision: ", division)
+
+#Ejercicio 5
+nombre = "Florencia"
+print("Nombre: ", nombre)
+
+#Ejercicio 6
+precio = 8576.7
+print("Precio: ", precio)
+
+#Ejercicio 7
+descuento = 0.35
+
+#Ejercicio 8
+precio_final = precio - (precio * descuento)
+print("Precio final: ", precio_final)
+
+#Ejercicio 9
+cadena = "Hola, como estas?"
+print("Cadena: " , cadena)
+
+#Ejercicio 10
+longitud = len(cadena)
+print("Longitud: " , longitud)
+
+#Ejercicio 11
+precio = int(564.7)
+print("Precio: ", precio)
+
+#Ejercicio 12
+nombre = "Flor"
+apellido = "Gimenez"
+nombre_completo = nombre + " " + apellido
+print("Nombre completo: ", nombre_completo)
+
+#Ejercicio 13
+edad = 18
+edad += 5
+edad -= 10
+print("Edad: ", edad)
+
+#Ejercicio 14
+altura = 1.60
+altura *= 4
+altura /= 3
+print("Altura: ", altura)
+
+#Ejercicio 15
+nombre = "FLORENCIA"
+nombre = nombre.lower()
+print("Nombre: ", nombre)
+
+#Ejercicio 16
+nombre = nombre.capitalize()
+print("Nombre: ", nombre)
---
+++ Ejercicio en clase for - while.py
@@ -0,0 +1,42 @@
+#Ejercicio 1 - for
+abecedario = ['a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+mensajes = []
+for i in range(2):
+    mensajes.append(input(f'Ingrese el mensaje num {i}: '))
+
+
+corrimiento = int(input(f'Indique el corrimiento: '))
+
+mensajes_encrip = []
+
+for mensaje in mensajes:
+    palabra_temporal = ""
+    for letra in mensaje:
+        if letra in abecedario:
+            indice = abecedario.index(letra)
+            nuevo_indice = (indice + corrimiento) % 27
+            letra_temporal = abecedario[nuevo_indice]
+            palabra_temporal += letra_temporal
+        else:
+            palabra_temporal += letra
+    mensajes_encrip.append(palabra_temporal)  
+
+
+for mensaje in mensajes_encrip:
+    print(mensaje)  
+    
+#Ejercicio 2 - while
+peers = 0
+odd = 0
+num = 1
+
+while num > 0:
+    num = int(input("Ingrese un numero mayor a 0: "))
+    if num % 2 == 0:
+        peers += 1
+    else:
+        odd += 1
+        
+print(f"La cantidad de numeros pares ingresados: {peers}") 
+print(f"La cantidad de numeros impares ingresados: {odd}")
