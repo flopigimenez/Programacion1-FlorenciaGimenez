b671fcd9a99c81ac68b9953bfee46abe2a43b901
flopigimenez <flopigmzm@gmail.com>
2023-10-18 02:16:08 -0300

    orden tps

--- Iteraciones/TP3.py
+++ Iteraciones/TP3.py
@@ -38,6 +38,19 @@ número de años, y muestre por pantalla el capital obtenido en la inversión ca
 """6-	Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo 
 rectángulo como el de más abajo, de altura el número introducido."""
 
+"""7-	Escribir un programa que muestre por pantalla las tablas de multiplicar del 1 al 10."""
+
+"""8-	Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo 
+rectángulo como el de más abajo."""
+ 
+"""9-	Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al 
+usuario por la contraseña hasta que introduzca la contraseña correcta."""
+
+"""10-	Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un número 
+primo o no."""
+
+"""11-	Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las 
+letras de la palabra introducida empezando por la última."""
 
 """12-	Escribir un programa en el que se pregunte al usuario por una frase y una letra, 
 y muestre por pantalla el número de veces que aparece la letra en la frase.""" 
@@ -93,3 +106,39 @@ print(f"Los divisores de {number} son:", end= " ")
 for i in range(1,number+1):
     if number % i == 0:
         print(i, end= " ")"""
+        
+"""16-	Escriba un programa que pregunte cuántos números se van a introducir, pida esos números y escriba 
+cuántos negativos ha introducido."""
+
+"""17-	Solicitar al usuario que ingrese una frase y luego imprimir un listado de las vocales que aparecen en 
+esa frase (sin repetirlas)."""
+
+"""18-	Crear un algoritmo que muestre los primeros 10 números de la sucesión de Fibonacci. La sucesión comienza 
+con los números 0 y 1 y, a partir de éstos, cada elemento es la suma de los dos números anteriores en la 
+secuencia: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55…"""
+
+"""19-	Escriba un programa que simule una alcancía. El programa solicitará primero una cantidad, que será la 
+cantidad de dinero que queremos ahorrar. A continuación, el programa solicitará una y otra vez las cantidades 
+que se irán ahorrando, hasta que el total ahorrado iguale o supere al objetivo. El programa deberá comprobar 
+que las cantidades ingresadas sean positivas."""
+
+"""20-	Leer números enteros de teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la sumatoria de 
+todos los números ingresados."""
+
+"""21-	Leer números enteros positivos de teclado, hasta que el usuario ingrese el 0. Informar cuál fue el mayor 
+número ingresado."""
+
+"""22-	Solicitar al usuario que ingrese números enteros positivos y, por cada uno, imprimir la suma de los 
+dígitos que lo componen. La condición de corte es que se ingrese el número -1. Al finalizar, mostrar cuántos 
+de los números ingresados por el usuario fueron números pares."""
+
+"""23-	Crear un programa que permita al usuario ingresar los montos de las compras de un cliente (se desconoce 
+la cantidad de datos que cargará, la cual puede cambiar en cada ejecución), cortando el ingreso de datos 
+cuando el usuario ingrese el monto 0."""
+
+"""24-	Si ingresa un monto negativo, no se debe procesar y se debe pedir que ingrese un nuevo monto. Al 
+finalizar, informar el total a pagar teniendo que cuenta que, si las ventas superan el total de $1000, 
+se le debe aplicar un 10% de descuento."""
+
+"""25-	Dado un número entero positivo, mostrar su factorial. El factorial de un número se obtiene multiplicando 
+todos los números enteros positivos que hay entre el 1 y ese número. El factorial de 0 es 1."""
---
+++ Tipos de datos/TP1-Parte2.py
@@ -0,0 +1,101 @@
+"""1.	Calcular el perímetro y área de un rectángulo dada su base y su altura."""
+#Ejercicio 1
+"""base = 5
+height = 15
+
+perimeter = 2* (base + height)
+area = base * height
+
+print(f"El perimetro de un rectangulo de base {base} y de altura {height} es {perimrter}")
+print(f"El area de un rectangulo de base {base} y de altura {heigth} es {area}")"""
+
+"""2.  Dados los catetos de un triángulo rectángulo, calcular su hipotenusa."""
+#Eercicio 2
+"""leg1 = 3
+leg2 = 4
+
+hypotenuse = (leg1**2 + leg2**2) ** (1/2)
+print(f"La hipotenusa de un triangulo rectangulo de catetos {leg1} y {leg2} es {hypotenuse}")"""
+
+"""3.	Dados dos números, mostrar la suma, resta, división y multiplicación de ambos."""
+#Ejercicio 3
+"""number1 = 7
+number2 = 4
+
+print(f"Suma entre {number1} y {number2}: ", number1 + number2)
+print(f"Resta entre {number1} y {number2}: ", number1 - number2)
+print(f"Multiplicacion entre {number1} y {number2}: ", number1 * number2)
+print(f"Division entre {number1} y {number2}: ", number1 / number2)"""
+
+"""4.	Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius. 
+Recordar que la fórmula para la conversión es: c=(f-32)*5/9"""
+#Ejercicio 4
+"""degrees_Fahrenheit = 30
+degrees_Celsius = (degrees_Fahrenheit - 32)*5/9
+print(f"{degrees_Fahrenheit} grados Fahrenheit son {degrees_Celsius} grados Celsius")"""
+ 
+"""5.	¿Qué problemas tienen las siguientes instrucciones?¿Cómo las solucionarías?
+a)	A = input(nombre, “¿Cuál es tu canción favorita?”)
+b)	precio = input(“Precio: “)
+total = precio + (precio * 0.1)
+c)	edad = int(input(“Edad: “))
+print(tu edad es, edad)
+d)	edad = int(input(“Edad: “))
+print(“Veamos si tu edad es 18…”, edad=18)"""
+#Ejemplo 5
+
+"""6.	Calcular la media de tres números pedidos por teclado."""
+
+"""7.	Realiza un programa que reciba una cantidad de minutos y muestre por pantalla a cuantas horas 
+y minutos corresponde. Por ejemplo: 1000 minutos son 16 horas y 40 minutos."""
+
+"""8.	Un vendedor recibe un sueldo base mas un 10% extra por comisión de sus ventas, el vendedor 
+desea saber cuanto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes 
+y el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones."""
+
+"""9.	Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente desea saber 
+cuanto deberá pagar finalmente por su compra."""
+
+"""10.	Un alumno desea saber cual será su calificación final en la materia de Algoritmos. Dicha 
+calificación se compone de los siguientes porcentajes:
+•	    55% del promedio de sus tres calificaciones parciales.
+•	    30% de la calificación del examen final.
+•	    15% de la calificación de un trabajo final."""
+
+"""11.	Pide al usuario dos números y muestra la “distancia” entre ellos (el valor absoluto de su 
+diferencia, de modo que el resultado sea siempre positivo)."""
+
+"""12.	Realizar un algoritmo que lea un número y que muestre su raíz cuadrada y su raíz cúbica."""
+
+"""13.	Dado un número de dos cifras, diseñe un algoritmo que permita obtener el número invertido. 
+Ejemplo, si se introduce 23 que muestre 32."""
+
+"""14.	Dadas dos variables numéricas A y B, que el usuario debe teclear, se pide realizar un algoritmo 
+que intercambie los valores de ambas variables y muestre cuanto valen al final las dos variables."""
+
+"""15.	Un ciclista parte de una ciudad A a las HH horas, MM minutos y SS segundos. El tiempo de viaje hasta 
+llegar a otra ciudad B es de T segundos. Escribir un algoritmo que determine la hora de llegada a la ciudad B."""
+
+"""16.	Pedir el nombre y los dos apellidos de una persona y mostrar las iniciales."""
+
+"""17.	Solicitar al usuario que ingrese su nombre. El nombre se debe almacenar en una variable llamada 
+usuario. A continuación mostrar por pantalla: “Ahora estás en la matrix, [nombre del usuario]”."""
+
+"""18.	Hacer un programa que solicite al usuario cuánto costó una cena en un restaurante. A ese valor, 
+sumarle un 6.2% en concepto de servicio y un 10% de propina. Imprimir en pantalla el monto final a pagar."""
+
+"""19.	Solicitar al usuario que ingrese el día, mes y año de su nacimiento y almacenar cada uno de ellos 
+en una variable numérica (en total, tres variables diferentes). Finalmente, mostrar la fecha en formato 
+dd/mm/aaaa."""
+
+"""20.	Hacer otra versión del programa, pero esta vez almacenado todo en una única variable con formato DDMMAAA."""
+
+"""21.	Una pareja de motociclistas necesita hacer ciertos cálculos antes de emprender un viaje en moto, para 
+saber cuántos tanques de combustible consumirá el viaje entero.
+Para eso deben ingresar: cuántos kilómetros puede recorrer su moto con 1 litro de combustible, qué capacidad 
+(en litros) tiene el tanque y cuántos kilómetros en total recorrerán.
+Hacer un programa que solicite los datos necesarios y luego informe la cantidad de tanques de combustible 
+necesarios."""
+
+
+
