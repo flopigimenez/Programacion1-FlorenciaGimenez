02c0a3002a92e88192b98f83a94de69c7135c9fe
Puerta Candela <candepuerta@gmail.com>
2023-10-30 12:42:20 -0300

    Se agregan algunos ej faltantes

--- Trabajos Practicos/TP6.py
+++ Trabajos Practicos/TP6.py
@@ -52,3 +52,56 @@ for i in list_numbers:
 
 print("Lista: ", end = "")    
 Funciones.show_list(new_list)"""
+
+"""5. Generar e imprimir una nueva lista que contenga como elementos a tuplas, cada una compuesta por un número de la lista original y la cantidad de veces que aparece en ella.
+ Por ejemplo, si la lista original es [5,16,2,5,57,5,2], la nueva lista contendrá: [(5,3),(16,1),(2,2),(57,1)]"""
+
+"""list_for_tuple = []
+for number in new_list:
+   numero = new_list.count(number)
+   new_tuple = (number,numero)
+   list_for_tuple.append(new_tuple)
+
+unique_tuples = []
+for number in set(new_list):
+    count = new_list.count(number)
+    unique_tuples.append((number, count))
+
+print(unique_tuples)"""
+
+"""6. Solicitar al usuario que ingrese los nombres de pila de los alumnos de nivel primario de una escuela, finalizando al ingresar ‘x’. A continuación, 
+solicitar que ingrese los nombres de los alumnos de nivel secundario, finalizando al ingresar ‘x’.
+a. Informar los nombres de todos los alumnos de nivel primario y de los de nivel secundario, sin repeticiones.
+b. Informar qué nombres se repiten entre los alumnos de nivel primario y secundario.
+c. Informar qué nombres de nivel primario no se repiten en los de nivel secundario.
+"""
+
+"""students_primary = []
+students_secondary = []
+while(True):
+    student_p = input("Ingrese el nombre de pila de los estudiantes de nivel primario, cuando quiera salir ingrese X: ")
+    if student_p == "x":
+        break
+    else:
+        students_primary.append(student_p)
+while(True):
+    student_s = input("Ingrese el nombre de pila de los estudiantes de nivel secundario, cuando quiera salir ingrese X: ")
+    if student_s == "x":
+        break
+    else:
+        students_secondary.append(student_s)
+all_students =students_primary + students_secondary
+unique_students = list(set(all_students))
+print("Nombres de todos los alumnos de nivel primario y secundario (sin repeticiones):")
+print(unique_students)
+duplicates = [name for name in unique_students if all_students.count(name) > 1]
+print("Nombres que se repiten entre los alumnos de nivel primario y secundario:")
+print(duplicates)
+
+unique_primary = list(set(students_primary))
+unique_secondary = list(set(students_secondary))
+names_unique_to_primary = [name for name in unique_primary if name not in unique_secondary]
+print("Nombres de nivel primario que no se repiten en los de nivel secundario:")
+print(names_unique_to_primary)
+
+"""
